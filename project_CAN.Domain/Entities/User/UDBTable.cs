00453d13945de9956d63b22a48aa9962899b8299
Cristian <smartcristian2002@gmail.com>
2024-04-05 08:32:38 +0300

    Update UDBTable.cs

--- project_CAN.Domain/Entities/User/UDBTable.cs
+++ project_CAN.Domain/Entities/User/UDBTable.cs
@@ -5,15 +5,16 @@ using project_CAN.Domain.Enums;
 
 namespace project_CAN.Domain.Entities.User
 {
+    [Table("userTable")]
     public class UDBTable
     {
         [Key]
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
-        public int userID { get; set; }
+        public int userId { get; set; }
 
         [Required]
         [Display(Name = "Username")]
-        [StringLength(30, MinimumLength = 5, ErrorMessage = "Username cannot be longer than 30 characters.")]
+        [StringLength(50, MinimumLength = 5, ErrorMessage = "Username cannot be longer than 50 characters.")]
         public string userName { get; set; }
 
         [Required]
@@ -23,13 +24,19 @@ namespace project_CAN.Domain.Entities.User
 
         [Required]
         [Display(Name = "Email Address")]
-        [StringLength(30)]
+        [StringLength(256)]
         public string email { get; set; }
 
+        [Required]
+        [Column(TypeName = "int")]
         public URole privilegies { get; set; }
 
+        public int? tutorialId { get; set; }
+
+        [Required]
         public bool isBlocked { get; set; }
 
-        public DateTime lastLogin {  get; set; }
+        [Required]
+        public DateTime lastLogin { get; set; }
     }
 }
